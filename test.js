e3dbef3f31a6769f3ed34ec4e5ca43b215eeff4e
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-09-10 10:19:00 +0200

    Recompile sample

--- test.js
+++ test.js
@@ -3,6 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.sum = sum;
 exports.factorial = factorial;
 exports.parseNum_parseTokens = parseNum_parseTokens;
 exports.parseNum = parseNum;
@@ -10,13 +11,39 @@ exports.parseTokens = parseTokens;
 
 var _fableCore = require("fable-core");
 
+function sum(v, m, s) {
+  _letrec: while (true) {
+    if (v >= m) {
+      return s;
+    } else {
+      var $tmp0 = v + 1;
+      var $tmp1 = m;
+      var $tmp2 = s + v;
+      v = $tmp0;
+      m = $tmp1;
+      s = $tmp2;
+      continue _letrec;
+    }
+  }
+}
+
+_fableCore.String.fsFormat("sum 0 10000L 0 -> %d")(function (x) {
+  console.log(x);
+})(sum(0, 10000, 0));
+
+_fableCore.String.fsFormat("(must be equal to 49995000)")(function (x) {
+  console.log(x);
+});
+
 function factorial(aux, n) {
   _letrec: while (true) {
     if (n === 0) {
       return aux;
     } else {
-      aux = aux * n;
-      n = n - 1;
+      var $tmp0 = aux * n;
+      var $tmp1 = n - 1;
+      aux = $tmp0;
+      n = $tmp1;
       continue _letrec;
     }
   }
@@ -29,8 +56,10 @@ function parseNum_parseTokens(_letrecFunction, tokens, acc, _arg1, _arg2) {
         var $target1 = function $target1() {
           return _arg2.tail == null ? _fableCore.List.reverse(tokens) : function () {
             _letrecFunction = "parseTokens";
-            tokens = tokens;
-            _arg2 = _arg2.tail;
+            var $tmp0 = tokens;
+            var $tmp1 = _arg2.tail;
+            tokens = $tmp0;
+            _arg2 = $tmp1;
           }();
         };
 
@@ -40,9 +69,14 @@ function parseNum_parseTokens(_letrecFunction, tokens, acc, _arg1, _arg2) {
             var xs = _arg2.tail;
             {
               _letrecFunction = "parseNum";
-              tokens = tokens;
-              acc = _fableCore.List.ofArray([x]);
-              _arg1 = xs;
+              var $tmp0 = tokens;
+
+              var $tmp1 = _fableCore.List.ofArray([x]);
+
+              var $tmp2 = xs;
+              tokens = $tmp0;
+              acc = $tmp1;
+              _arg1 = $tmp2;
               continue _letrec;
             }
           } else {
@@ -57,29 +91,40 @@ function parseNum_parseTokens(_letrecFunction, tokens, acc, _arg1, _arg2) {
     } else {
       if (_letrecFunction === "parseNum") {
         {
-          var $target1 = function $target1() {
+          var _$target = function _$target() {
             _letrecFunction = "parseTokens";
-            tokens = _fableCore.List.ofArray([_fableCore.List.reverse(acc)], tokens);
-            _arg2 = _arg1;
+
+            var $tmp0 = _fableCore.List.ofArray([_fableCore.List.reverse(acc)], tokens);
+
+            var $tmp1 = _arg1;
+            tokens = $tmp0;
+            _arg2 = $tmp1;
           };
 
           if (_arg1.tail != null) {
             if (_arg1.head >= "0" ? _arg1.head <= "9" : false) {
-              var x = _arg1.head;
-              var xs = _arg1.tail;
+              var _x = _arg1.head;
+              var _xs = _arg1.tail;
               {
                 _letrecFunction = "parseNum";
-                tokens = tokens;
-                acc = _fableCore.List.ofArray([x], acc);
-                _arg1 = xs;
+                var _$tmp = tokens;
+
+                var _$tmp2 = _fableCore.List.ofArray([_x], acc);
+
+                var _$tmp3 = _xs;
+                tokens = _$tmp;
+                acc = _$tmp2;
+                _arg1 = _$tmp3;
                 continue _letrec;
               }
             } else {
-              $target1();
+              _$target();
+
               continue _letrec;
             }
           } else {
-            $target1();
+            _$target();
+
             continue _letrec;
           }
         }
